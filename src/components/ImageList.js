9cd8363341423abe044042f7ae87ca83779d3b70
AhmedKhatri510 <ahmedkhatri510@gmail.com>
2021-09-20 22:51:36 +0530

    added images in grid fashion

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,13 +1,15 @@
 import React from "react";
+import "./ImageList.css";
+import ImageCard from "./ImageCard";
 
 const ImageList = (props) => {
   // console.log(props.images);
-  const images = props.images.map(({ id, description, urls }) => {
-    return <img key={id} alt={description} src={urls.regular} />;
+  const images = props.images.map((image) => {
+    return <ImageCard key={image.id} image={image} />;
   });
   console.log(images);
 
-  return <div>{images}</div>;
+  return <div className="image-list">{images}</div>;
 };
 
 export default ImageList;
